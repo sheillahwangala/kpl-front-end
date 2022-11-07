05f436bce387604a3714de1b1d91663813a59d85
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-07 20:31:48 +0300

    update

--- src/components/team/EachTeam.js
+++ src/components/team/EachTeam.js
@@ -1,51 +1,51 @@
-// import React, { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-// function EachTeam() {
+function EachTeam() {
 
-//   const params = useParams();
+  const params = useParams();
 
-//   console.log(params.id);
+  console.log(params.id);
 
-//   const [teams, setTeams] = useState([]);
+  const [teams, setTeams] = useState([]);
 
-//   useEffect(() => {
-//     fetch ("http://127.0.0.1:9292/teams")
-//     .then ((response) => response.json())
-//     .then((data) => {
-//       setTeams(data)
-//     })   
-//   }, []);
+  useEffect(() => {
+    fetch ("http://127.0.0.1:9292/teams")
+    .then ((response) => response.json())
+    .then((data) => {
+      setTeams(data)
+    })   
+  }, []);
 
-//   // fetch("http://127.0.0.1:9292/teams")
-//   //   .then((response) => response.json())
-//   //   .then((data) => {
-//   //     setTeams(data);
-//   //   }, []);
+  // fetch("http://127.0.0.1:9292/teams")
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     setTeams(data);
+  //   }, []);
 
-//   const showEachTeam = teams.map(
-//     (team) =>
-//       team.id === parseInt(params.id) && (
-//         <div>
-//           <h1>{team.name}</h1>
-//           <p>{team.coach}</p>
-//           {team.players.map((player) => (
-//             <div>              
-//               <h2>Name: {player.name}</h2>
-//               <h3>Position: {player.position}</h3>
+  const showEachTeam = teams.map(
+    (team) =>
+      team.id === parseInt(params.id) && (
+        <div>
+          <h1>{team.name}</h1>
+          <p>{team.coach}</p>
+          {team.players.map((player) => (
+            <div>              
+              <h2>Name: {player.name}</h2>
+              <h3>Position: {player.position}</h3>
               
-//             </div>
-//           ))}
-//         </div>
-//       )
-//   );
+            </div>
+          ))}
+        </div>
+      )
+  );
 
-//   return (
-//     <div>
-//       EachTeam
-//       {showEachTeam}
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      EachTeam
+      {showEachTeam}
+    </div>
+  );
+}
 
-// export default EachTeam;
+export default EachTeam;

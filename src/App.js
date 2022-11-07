03a8840d1cb3cbff8c1e4f5a32359648657c7940
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-07 13:31:45 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
-import {BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Team from "./components/team/Team";
+import Player from "./components/player/Player"
+import Nav from "./components/nav/Nav"
 
 function App() {
+
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Template>
-          <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/teams" element={<TeamsPage />} />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Team />} />
+          <Route path="/teams" element={<Team />} />
           <Route path="/player" element={<Player />} />
-          <Route path="/team-stats" element={<TeamStats />} />
-          </Routes>
-        </Template>
+          {/* <Route path="/team-stats" element={<TeamStats />} /> */}
+        </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }

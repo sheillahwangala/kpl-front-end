c38a96a31c9ae4d8e948f0ec19f13f09a5d00d58
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-07 20:02:41 +0300

    update

--- src/components/team/EachTeam.js
+++ src/components/team/EachTeam.js
@@ -1,37 +1,51 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
 
-function EachTeam() {
-  const params = useParams();
-  console.log(params.id);
-  const [teams, setTeams] = useState([]);
-  fetch("http://127.0.0.1:9292/teams")
-    .then((response) => response.json())
-    .then((data) => {
-      setTeams(data);
-    }, []);
+// function EachTeam() {
 
-  const showEachTeam = teams.map(
-    (team) =>
-      team.id === parseInt(params.id) && (
-        <div>
-          <h1>{team.name}</h1>
-          {team.players.map((player) => (
-            <div>
-              <h2>{player.name}</h2>
-              <h3>{player.position}</h3>
-            </div>
-          ))}
-        </div>
-      )
-  );
+//   const params = useParams();
 
-  return (
-    <div>
-      EachTeam
-      {showEachTeam}
-    </div>
-  );
-}
+//   console.log(params.id);
 
-export default EachTeam;
+//   const [teams, setTeams] = useState([]);
+
+//   useEffect(() => {
+//     fetch ("http://127.0.0.1:9292/teams")
+//     .then ((response) => response.json())
+//     .then((data) => {
+//       setTeams(data)
+//     })   
+//   }, []);
+
+//   // fetch("http://127.0.0.1:9292/teams")
+//   //   .then((response) => response.json())
+//   //   .then((data) => {
+//   //     setTeams(data);
+//   //   }, []);
+
+//   const showEachTeam = teams.map(
+//     (team) =>
+//       team.id === parseInt(params.id) && (
+//         <div>
+//           <h1>{team.name}</h1>
+//           <p>{team.coach}</p>
+//           {team.players.map((player) => (
+//             <div>              
+//               <h2>Name: {player.name}</h2>
+//               <h3>Position: {player.position}</h3>
+              
+//             </div>
+//           ))}
+//         </div>
+//       )
+//   );
+
+//   return (
+//     <div>
+//       EachTeam
+//       {showEachTeam}
+//     </div>
+//   );
+// }
+
+// export default EachTeam;

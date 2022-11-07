367a70e2c374375eab56441645d9079121b11a1f
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-07 13:46:48 +0300

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Team from "./components/team/Team";
 import Player from "./components/player/Player"
 import Nav from "./components/nav/Nav"
+import TeamStats from "./components/team/TeamStats";
+import PlayersStats from "./components/player/PlayersStats";
 
 function App() {
 
@@ -13,9 +14,9 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Team />} />
-          <Route path="/teams" element={<Team />} />
           <Route path="/player" element={<Player />} />
-          {/* <Route path="/team-stats" element={<TeamStats />} /> */}
+          <Route path="/team-stats" element={<TeamStats />} />
+          <Route path ="/players-stats" element={<PlayersStats/>} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/player/PlayersStats.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PlayersStats() {
+  return (
+    <div>PlayersStats</div>
+  )
+}
+
+export default PlayersStats

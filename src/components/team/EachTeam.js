a4dc86dfe0775d598cefe39697c66314f25f5d8a
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-07 20:33:23 +0300

    update

--- src/components/team/EachTeam.js
+++ src/components/team/EachTeam.js
@@ -1,38 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function EachTeam() {
-
   const params = useParams();
-
-  console.log(params.id);
-
+  // console.log(params.id);
   const [teams, setTeams] = useState([]);
 
-  useEffect(() => {
-    fetch ("http://127.0.0.1:9292/teams")
-    .then ((response) => response.json())
+  useEffect(() =>{
+    fetch("http://127.0.0.1:9292/teams")
+    .then((response) => response.json())
     .then((data) => {
-      setTeams(data)
-    })   
-  }, []);
+      setTeams(data);
+    }, []);
+  })
 
-  // fetch("http://127.0.0.1:9292/teams")
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     setTeams(data);
-  //   }, []);
 
   const showEachTeam = teams.map(
     (team) =>
       team.id === parseInt(params.id) && (
         <div>
           <h1>{team.name}</h1>
-          <p>{team.coach}</p>
           {team.players.map((player) => (
-            <div>              
-              <h2>Name: {player.name}</h2>
-              <h3>Position: {player.position}</h3>
+            <div>
+              <img src={player.image}/>
+              <h2>{player.name}</h2>
+              <h3>{player.position}</h3>
+              <p>{player.goals}</p>
+              <p>{player.assists}</p>
               
             </div>
           ))}

48ca77cede1c45a49d3c6264cfc3d691394a34a9
Realmunene <josep.mwangi@student.moringaschool.com>
2022-11-07 21:22:14 +0300

    Add player form

--- src/components/player/AddPlayer.js
+++ src/components/player/AddPlayer.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function AddPlayer() {
+ const [name, setName]= useState("");
+ const [image, setImage] = useState("");
+ const [position, setPosition] = useState("");
   return (
-    <div>AddPlayer</div>
+    <div>
+      <div>AddPlayer</div>
+    <div>
+      <form >
+        <input type="text" placeholder ="Player name" value={name} onChange={(e)=>setName(e.target.value)} />
+        <input type="text" placeholder ="Image" value={image} onChange={(e)=>setImage(e.target.value)} />
+        <input type="text" placeholder ="position" value={position} onChange={(e)=>setPosition(e.target.value)} />
+      </form>
+    </div>
+    
+    </div>
   )
 }
 

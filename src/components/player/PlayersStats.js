70fcc459554e9f94612d70623a262910567dd4b5
Realmunene <josep.mwangi@student.moringaschool.com>
2022-11-09 02:28:14 +0300

    Done some fetch to playerStats

--- src/components/player/PlayersStats.js
+++ src/components/player/PlayersStats.js
@@ -5,21 +5,22 @@ function PlayersStats() {
   const [playerStat, setplayerStat] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:9292")
+    fetch("http://127.0.0.1:9292/players")
       .then((response) => response.json())
       .then((data) => setplayerStat(data));
   }, []);
 
-  // const playerStatistics = playerStat.map((playerSta) => (
-  //   // <div key={topTeam.id} className="container">
-  //   //   <h2>{}</h2>
-  //   //   <img src={} alt="logo" height="100px" />
-  //   //   <h5>{} </h5>
-  //   // </div>
-  // ));
+  const playerStatistics = playerStat.map((playerSta) => (
+    <div key={playerSta.id} className="container">
+      <h2>{playerSta.name}</h2>
+      <img src={playerSta.image} alt="logo" height="100px" />
+      <h5>{playerSta.goals} </h5>
+      <h5>{playerSta.assists} </h5>
+    </div>
+  ));
   return (
     <div>PlayersStats
-      {/* {playerStatistics} */}
+      {playerStatistics}
     </div>
   )
 }

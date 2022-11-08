d1e0e00fcddf94a05117437a1a82e44a242d8549
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-11-08 10:12:31 +0300

    added  create player function

--- src/components/player/AddPlayer.js
+++ src/components/player/AddPlayer.js
@@ -1,19 +1,39 @@
 import React, {useState} from 'react'
 
-function AddPlayer() {
+function AddPlayer({teamId}) {
  const [name, setName]= useState("");
  const [image, setImage] = useState("");
  const [position, setPosition] = useState("");
+ const [player, setPlayer] = useState([]);
+
+
+
+ const createPlayer = (e) => {
+  e.preventDefault();
+  fetch("http://127.0.0.1:9292/players", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      team_id: teamId,
+      name: name,
+      image: image,
+      position: position
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => setPlayer(data));
+};
+
   return (
     <div>
       <div>AddPlayer</div>
     <div>
-      <form >
         <input type="text" placeholder ="Player name" value={name} onChange={(e)=>setName(e.target.value)} />
         <input type="text" placeholder ="Image" value={image} onChange={(e)=>setImage(e.target.value)} />
         <input type="text" placeholder ="position" value={position} onChange={(e)=>setPosition(e.target.value)} />
-        <button type="submit">Add Player</button>
-      </form>
+        <button type="button" onClick={createPlayer}>Add Player</button>
     </div>
     
     </div>

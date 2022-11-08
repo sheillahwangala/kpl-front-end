0a038bb9028a89b02d13a3918c69ff42e8c6248c
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-08 19:05:17 +0300

    add update function

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -2,10 +2,13 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./player.css";
+
 function Player() {
   const params = useParams();
   console.log(params.id);
   const [players, setPlayers] = useState([]);
+  const [goals, setGoals] = useState("")
+  const [assists, setAssists] = useState("")
 
   useEffect(() => {
     fetch("http://127.0.0.1:9292/players")
@@ -17,15 +20,63 @@ function Player() {
     (player) =>
       player.id === Number(params.id) && (
         <div>
-          <p>{player.name}</p>
-          Goals: <p>{player.goals}</p>
-          Assists: <p>{player.assists}</p>
-          Position:<h3>{player.position}</h3>
+          <h4> Name: {player.name}</h4>
+          <h5>Position: {player.position}</h5>
+          <p>Goals: {player.goals}</p>
+          <p>Assists: {player.assists}</p>        
         </div>
       )
   );
 
-  return <div>{displayPlayer}</div>;
+  const updatePlayer = (e) => {
+    e.preventDefault();
+    fetch(`http://127.0.0.1:9292/players/${params.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        goals: goals,
+        assists: assists
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setPlayers(data);
+        setGoals("");
+        setAssists("");
+      })
+  };
+
+  const updatePlayerForm = (
+    <div>
+      <input
+        type="number"
+        name="goals"
+        placeholder="goals"
+        onChange={(e) => setGoals(e.target.value)}
+        value={goals}
+      />
+      <input
+        type="number"
+        name="assists"
+        placeholder="assists"
+        onChange={(e) => setAssists(e.target.value)}
+        value={assists}
+      />
+      <button type="button" onClick={updatePlayer}>
+        Update Player
+      </button>
+    </div>
+  );
+
+
+  return (
+      <div>
+        <div>{displayPlayer}</div>
+        {updatePlayerForm}     
+      </div>
+  )
 }
 
 export default Player;

6dc59c2e7cf0016c9a18deb7546b556f2daff384
Realmunene <josep.mwangi@student.moringaschool.com>
2022-11-09 00:45:17 +0300

    displaying the player statistics

--- src/components/player/PlayersStats.js
+++ src/components/player/PlayersStats.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useState, useEffect } from "react";
 
 function PlayersStats() {
+  const [playerStat, setplayerStat] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:9292")
+      .then((response) => response.json())
+      .then((data) => setplayerStat(data));
+  }, []);
+
+  // const playerStatistics = playerStat.map((playerSta) => (
+  //   // <div key={topTeam.id} className="container">
+  //   //   <h2>{}</h2>
+  //   //   <img src={} alt="logo" height="100px" />
+  //   //   <h5>{} </h5>
+  //   // </div>
+  // ));
   return (
-    <div>PlayersStats</div>
+    <div>PlayersStats
+      {/* {playerStatistics} */}
+    </div>
   )
 }
 

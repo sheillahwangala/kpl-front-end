9edd3a2fefaa2de861689af3127bfc0ad98192cc
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-11-08 21:57:34 +0300

    fetch top scoring teams data

--- src/components/team/TeamStats.js
+++ src/components/team/TeamStats.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 function TeamStats() {
+  const [topTeams, setTopTeams] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:9292/top_scoringteams")
+      .then((response) => response.json())
+      .then((data) => setTopTeams(data));
+  }, []);
   return (
     <div>TeamStats</div>
   )

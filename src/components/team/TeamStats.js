14a567bab74e2607e51dedc5548d5f8f3beea70e
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-11-08 22:00:15 +0300

    display top teams data

--- src/components/team/TeamStats.js
+++ src/components/team/TeamStats.js
@@ -8,6 +8,14 @@ function TeamStats() {
       .then((response) => response.json())
       .then((data) => setTopTeams(data));
   }, []);
+
+  const displayTopTeams = topTeams.map((topTeam) => (
+    <div key={topTeam.id} className="container">
+      <h2>{topTeam.name}</h2>
+      <img src={topTeam.logo} alt="logo" height="100px" />
+      <h5>{topTeam.coach} </h5>
+    </div>
+  ));
   return (
     <div>TeamStats</div>
   )

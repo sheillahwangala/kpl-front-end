ec48cdd1aabfb5ddbf83bbfdeff2189e06a04e24
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-09 14:57:57 +0300

    update team component

--- src/components/team/Teams.js
+++ src/components/team/Teams.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+// import EachTeam from "./EachTeam";
 import NewTeamForm from "./NewTeamForm";
 
 import "./team.css";
@@ -14,20 +15,54 @@ function Teams() {
       .then((data) => setTeams(data));
   }, []);
 
-  const displayTeams = teams.map((team) => (
-    <div key={team.id} className="container">
-      <h2>{team.name}</h2>
-      <img src={team.logo} alt="logo" height="100px" />
-      <h5>{team.coach} </h5>
-      <Link to={`/teams/${team.id}`}>See Team</Link>
-    </div>
-  ));
+  // const displayTeams = teams.map((team) => (
+  //   <div key={team.id} className="container">
+  //     <h2>{team.name}</h2>
+  //     <img src={team.logo} alt="logo" height="100px" />
+  //     <h5>{team.coach} </h5>
+  //     <Link to={`/teams/${team.id}`}>See Team</Link>
+  //   </div>
+  // ));
 
   return (
     <div>
-      {displayTeams}
-      <NewTeamForm setTeams={setTeams} />
+      {/* {displayTeams} */}     
+
+      <table className="table table-striped" >
+        <tr>
+          <th>Team Name</th>
+          <th>Team Logo</th>
+          <th>Games Played</th>
+          <th>Won</th>
+          <th>Drawn</th>
+          <th>Lost</th>
+          <th>Points</th>
+        </tr>
+        {teams.map((team, key) => {
+          return (
+            <tr key={team.id} >
+              <td>
+              <Link to={`/teams/${team.id}`}>{team.name}</Link>
+              </td>
+              <td>
+              <img src={team.logo} alt="logo" height="100px" />
+              </td>
+              <td>{team.matches_played}</td>
+              <td>{team.matches_won}</td>
+              <td>{team.matches_drawn}</td>
+              <td>{team.matches_lost}</td>
+              <td>{team.points}</td>
+            </tr>
+          )
+        })}
+
+      </table> 
+
+      <NewTeamForm setTeams={setTeams} /> 
+        
     </div>
+
+
   );
 }
 

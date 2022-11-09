52e067f00409dc4ae10cc1b87595b4be10d2041e
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-09 21:09:30 +0300

    update players

--- src/components/player/Players.js
+++ src/components/player/Players.js
@@ -11,16 +11,16 @@ function Players() {
       .then((data) => setPlayers(data));
   }, []);
 
-  const displayPlayers = players.map((player) => (
-    <div key={player.id}>
-      <h2>{player.name}</h2>
-      <h2>{player.image}</h2>
-      <h2>{player.position}</h2>
-      <h2>{player.goals}</h2>
-      <h2>{player.assists}</h2>
-      <Link to={`/players/${player.id}`}>See Player</Link>
-    </div>
-  ));
+  // const displayPlayers = players.map((player) => (
+  //   <div key={player.id}>
+  //     <h2>{player.name}</h2>
+  //     <h2>{player.image}</h2>
+  //     <h2>{player.position}</h2>
+  //     <h2>{player.goals}</h2>
+  //     <h2>{player.assists}</h2>
+  //     <Link to={`/players/${player.id}`}>See Player</Link>
+  //   </div>
+  // ));
   return (
     // <div>{displayPlayers}</div>;
     <div className="container" >
@@ -35,20 +35,19 @@ function Players() {
         <th>Goals</th>
         <th>Assists</th>
       </tr>
-      {teams.map((team, key) => {
+      {players.map((player) => {
         return (
-          <tr key={team.id} >
+          <tr key={player.id} >
+            <td>1</td>
             <td className="team-name" >
-              <Link className="text-decoration-none text-dark" to={`/teams/${team.id}`}>{team.name}</Link>
+              <Link className="text-decoration-none text-dark" to={`/players/${player.id}`}>{player.name}</Link>
             </td>
             <td>
-              <img src={team.logo} alt="logo" height="100px" />
+              <img src={player.image} alt="img" height="100px" />
             </td>
-            <td>{team.matches_played}</td>
-            <td>{team.matches_won}</td>
-            <td>{team.matches_drawn}</td>
-            <td>{team.matches_lost}</td>
-            <td>{team.points}</td>
+            <td>{player.position}</td>
+            <td>{player.goals}</td>
+            <td>{player.assists}</td>
           </tr>
         )
       })}

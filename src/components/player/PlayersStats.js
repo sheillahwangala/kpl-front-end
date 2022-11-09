e7574da6cf6149f9a2d2a5f1d8a94705a7a97e89
Realmunene <josep.mwangi@student.moringaschool.com>
2022-11-09 12:34:50 +0300

    modified table

--- src/components/player/PlayersStats.js
+++ src/components/player/PlayersStats.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./player.css"
 
 function PlayersStats() {
   const [playerGoals, setplayerGoals] = useState([]);
@@ -17,27 +18,53 @@ function PlayersStats() {
 
   const playerStatisticsGoals = playerGoals.map((playerGoal) => (
     <div key={playerGoal.id} className="container">
-      <h2>{playerGoal.name}</h2>
-      <img src={playerGoal.image} alt="logo" height="100px" />
-      <h5>{playerGoal.goals} </h5>
-      <h5>{playerGoal.assists} </h5>
-      <Link to={`/players/${playerGoal.id}`}>See Player</Link>
+      <table>
+        <tr>
+          <th>Name of player</th>
+          <th>Image</th>
+          <th>Team</th>
+          <th>Goals</th>
+        </tr>
+        <tr>
+          <td>{playerGoal.name}</td>
+          <td><img src={playerGoal.image} alt="logo" height="100px" /></td>
+          <td>{playerGoal.name}</td>
+          <td><h5>{playerGoal.goals} </h5></td>
+          </tr>
+      </table>
+        {/* <h5>{playerGoal.assists} </h5>
+        <Link to={`/players/${playerGoal.id}`}>See Player</Link> */}
+     
     </div>
   ));
   const playerStatisticsAssits = playerAssists.map((playerAsssist) => (
     <div key={playerAsssist.id} className="container">
-      <h2>{playerAsssist.name}</h2>
+      <table>
+        <tr>
+          <th>Name of player</th>
+          <th>Image</th>
+          <th>Team</th>
+          <th>Goals</th>
+        </tr>
+        <tr>
+          <td>{playerAsssist.name}</td>
+          <td><img src={playerAsssist.image} alt="logo" height="100px" /></td>
+          <td>{playerAsssist.name}</td>
+          <td><h5>{playerAsssist.assists}</h5></td>
+          </tr>
+      </table>
+      {/* <h2></h2>
       <img src={playerAsssist.image} alt="logo" height="100px" />
       goals <h5>{playerAsssist.goals} </h5>
       assists <h5>{playerAsssist.assists} </h5>
-      <Link to={`/players/${playerAsssist.id}`}>See Player</Link>
+      <Link to={`/players/${playerAsssist.id}`}>See Player</Link> */}
     </div>
   ));
   return (
-    <div>
-      Top goalscorers
+    <div className="playerStats">
+      <h2>Top goalscorers</h2>
       {playerStatisticsGoals}
-      top assisters
+      <h2>Top assisters</h2>
       {playerStatisticsAssits}
     </div>
   );
